51c909316b0645397b4d9aa2d8da8bf8fc5890c7
ankitgiri-gif <giriankit78319@gmail.com>
2026-01-15 16:16:40 +0530

    Add files via upload

---
+++ src/config/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAriySmgqKIl6KFt9Gf3GjBF3js_wEFErc",
+  authDomain: "yoga-master-31.firebaseapp.com",
+  projectId: "yoga-master-31",
+  storageBucket: "yoga-master-31.appspot.com",
+  messagingSenderId: "869554014628",
+  appId: "1:869554014628:web:bd7f097fcb31ab4dfc1931"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
